9baf8141a20d106e999481bfadd66aacbbb65d10
EdinK1 <edinkaymakchi@gmail.com>
2020-01-15 23:40:39 -0800

    get all the required data to show in schedule page

--- js/screens/Schedule/Schedule.js
+++ js/screens/Schedule/Schedule.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {useQuery} from '@apollo/react-hooks';
+import {gql} from 'apollo-boost';
+
+const ALL_SESSIONS = gql`
+  {
+    allSessions {
+      id
+      title
+      location
+      startTime
+    }
+  }
+`;
 
 const Schedule = () => {
-  return <Text>Schedule</Text>;
+  const {loading, error, data} = useQuery(ALL_SESSIONS);
+  return loading ? (
+    <Text>loading...</Text>
+  ) : error ? (
+    <Text>something went wrong.</Text>
+  ) : (
+    data.allSessions.map(session => (
+      <View key={session.id}>
+        <Text>{session.title}</Text>
+        <Text>{session.location}</Text>
+        <Text>{session.startTime}</Text>
+      </View>
+    ))
+  );
 };
 
 export default Schedule;
