28cc88117222762ca57b6fd7e039d22d22ee07ea
EdinK1 <edinkaymakchi@gmail.com>
2020-01-16 00:13:32 -0800

    create map screen

--- js/App.js
+++ js/App.js
@@ -3,12 +3,14 @@ import {StyleSheet, Text} from 'react-native';
 import About from './screens/About';
 import ApolloWrapper from './components/Apollo';
 import Schedule from './screens/Schedule/Schedule';
+import Map from './screens/Map';
 
 const App = () => {
   return (
     <ApolloWrapper>
       {/* <About /> */}
-      <Schedule />
+      {/* <Schedule /> */}
+      <Map />
     </ApolloWrapper>
   );
 };
---
+++ js/screens/Map/Map.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import MapView from 'react-native-maps';
+import {StyleSheet, Dimensions} from 'react-native';
+
+const Map = () => (
+  <MapView
+    style={styles.mapStyle}
+    initialRegion={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+  />
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
+
+export default Map;
