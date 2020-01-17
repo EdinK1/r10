d74559a6dde4c37164fd236fb8d012adb23fd075
EdinK1 <edinkaymakchi@gmail.com>
2020-01-17 12:27:42 -0800

    add faves to navigation

--- js/navigation/Navigator.js
+++ js/navigation/Navigator.js
@@ -5,15 +5,18 @@ import About from '../screens/About/';
 import Schedule from '../screens/Schedule/';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import Map from '../screens/Map/';
-import {Image} from 'react-native';
+import Faves from '../screens/Faves/';
+
 const HomeScene = createStackNavigator({Schedule});
 const MapScene = createStackNavigator({Map});
 const AboutScene = createStackNavigator({About});
+const FavesScene = createStackNavigator({Faves});
 
 const Navigator = createBottomTabNavigator(
   {
     Schedule: HomeScene,
     Map: MapScene,
+    Faves: FavesScene,
     About: AboutScene,
   },
   {
---
+++ js/screens/Faves/Faves.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Faves = () => {
+  return <Text>faves</Text>;
+};
+
+export default Faves;
