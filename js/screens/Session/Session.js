96511b8cfcebec192281173b2fe65ee81ba382c5
EdinK1 <edinkaymakchi@gmail.com>
2020-01-19 16:54:44 -0800

    create session page

--- js/screens/Session/Session.js
+++ js/screens/Session/Session.js
@@ -2,7 +2,11 @@ import React from 'react';
 import {useQuery} from '@apollo/react-hooks';
 import {gql} from 'apollo-boost';
 import Section from '../../components/Section';
-import {Text} from 'react-native';
+import {Text, Image, TouchableOpacity} from 'react-native';
+import Paragraph from '../../components/Paragraph';
+import Heading from '../../components/Heading';
+import globalStyles from '../../assets/styles/styles';
+import FavoriteBtn from '../../components/FavoriteBtn';
 
 const getSelectedSessionById = id => {
   const {data, loading, error} = useQuery(
@@ -26,21 +30,36 @@ const getSelectedSessionById = id => {
     `,
     {variables: {id}},
   );
-  return {session: data ? data.Session : loading, error};
+  return {session: data ? data.Session : null, loading, error};
 };
 
 const Session = ({navigation}) => {
   const {error, loading, session} = getSelectedSessionById(
     navigation.getParam('id'),
   );
-  // console.log();
-  return (
+
+  return loading ? (
+    <Paragraph>loading...</Paragraph>
+  ) : error ? (
+    <Paragraph>Something went wrong</Paragraph>
+  ) : session ? (
     <Section>
-      <Text>
-        {session.title} | {session.description}
-      </Text>
+      <Text>{session.location}</Text>
+      <Heading>{session.title}</Heading>
+      <Text>{session.startTime}</Text>
+      <Paragraph>{session.description}</Paragraph>
+      <Text>Presented by:</Text>
+
+      <TouchableOpacity>
+        <Text>{session.speaker.name}</Text>
+        <Image
+          style={{width: 65, height: 65, borderRadius: 65 / 2}}
+          source={{uri: session.speaker.image}}
+        />
+      </TouchableOpacity>
+      <FavoriteBtn color="#9963ea" label="Add to Faves" />
     </Section>
-  );
+  ) : null;
 };
 
 export default Session;
