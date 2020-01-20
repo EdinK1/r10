1a8ad195be7c25d9db32c726088c6b2187c68e72
EdinK1 <edinkaymakchi@gmail.com>
2020-01-19 20:41:17 -0800

    add modal to session screen

--- js/screens/Session/Session.js
+++ js/screens/Session/Session.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useQuery} from '@apollo/react-hooks';
 import {gql} from 'apollo-boost';
 import Section from '../../components/Section';
 import {Text, Image, TouchableOpacity} from 'react-native';
 import Paragraph from '../../components/Paragraph';
 import Heading from '../../components/Heading';
-import globalStyles from '../../assets/styles/styles';
 import FavoriteBtn from '../../components/FavoriteBtn';
+import Modal from '../../components/Modal';
 
 const getSelectedSessionById = id => {
   const {data, loading, error} = useQuery(
@@ -34,6 +34,10 @@ const getSelectedSessionById = id => {
 };
 
 const Session = ({navigation}) => {
+  const [openModal, setOpenModal] = useState(false);
+
+  const toggleModal = () => setOpenModal(!openModal);
+
   const {error, loading, session} = getSelectedSessionById(
     navigation.getParam('id'),
   );
@@ -50,13 +54,22 @@ const Session = ({navigation}) => {
       <Paragraph>{session.description}</Paragraph>
       <Text>Presented by:</Text>
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={toggleModal}>
         <Text>{session.speaker.name}</Text>
         <Image
           style={{width: 65, height: 65, borderRadius: 65 / 2}}
           source={{uri: session.speaker.image}}
         />
       </TouchableOpacity>
+      <Modal open={openModal} onClose={toggleModal}>
+        <Image
+          style={{width: 65, height: 65, borderRadius: 65 / 2}}
+          source={{uri: session.speaker.image}}
+        />
+        <Text>{session.speaker.name}</Text>
+        <Text>{session.speaker.bio}</Text>
+        <Text>{session.speaker.url}</Text>
+      </Modal>
       <FavoriteBtn color="#9963ea" label="Add to Faves" />
     </Section>
   ) : null;
