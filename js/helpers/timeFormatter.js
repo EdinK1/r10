5ff3a214507ec05b2910c7a6d440e807c84e6af7
EdinK1 <edinkaymakchi@gmail.com>
2020-02-04 20:18:31 -0800

    fix time issue on android

--- js/helpers/timeFormatter.js
+++ js/helpers/timeFormatter.js
@@ -1,4 +1,17 @@
-const startTime = time =>
-  new Date(time).toLocaleString('en-US', {hour: 'numeric', hour12: true});
+// const startTime = time =>
+//   new Date(time).toLocaleString('en-US', {hour: 'numeric', hour12: true});
 
+// export default startTime;
+
+function startTime(time) {
+  const date = new Date(time);
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
 export default startTime;
